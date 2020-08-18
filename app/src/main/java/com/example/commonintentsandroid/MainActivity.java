eee7c9688bc4ca2108b6bb393db3864867c109ec
PKS <pkumarsaha71@gmail.com>
2020-08-19 02:14:17 +0600

    Edit text and buttons were initialized

--- app/src/main/java/com/example/commonintentsandroid/MainActivity.java
+++ app/src/main/java/com/example/commonintentsandroid/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.commonintentsandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText et_data;
+    Button btn_web,btn_email,btn_dail,btn_call,btn_sms,btn_maps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et_data = findViewById(R.id.et_data);
+        btn_web = findViewById(R.id.btn_web);
+        btn_email = findViewById(R.id.btn_email);
+        btn_dail = findViewById(R.id.btn_dail);
+        btn_call = findViewById(R.id.btn_call);
+        btn_sms = findViewById(R.id.btn_sms);
+        btn_maps = findViewById(R.id.btn_maps);
     }
 }

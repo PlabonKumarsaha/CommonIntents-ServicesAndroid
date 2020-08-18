f3280f0e55e6fa29c23d021969e86d207e6f071e
PKS <pkumarsaha71@gmail.com>
2020-08-19 02:45:25 +0600

    btn_web is completed

--- app/src/main/java/com/example/commonintentsandroid/MainActivity.java
+++ app/src/main/java/com/example/commonintentsandroid/MainActivity.java
@@ -2,9 +2,16 @@ package com.example.commonintentsandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DownloadManager;
+import android.app.SearchManager;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.util.Queue;
 
 public class MainActivity extends AppCompatActivity {
     EditText et_data;
@@ -22,5 +29,26 @@ public class MainActivity extends AppCompatActivity {
         btn_call = findViewById(R.id.btn_call);
         btn_sms = findViewById(R.id.btn_sms);
         btn_maps = findViewById(R.id.btn_maps);
+
+        btn_web.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = et_data.getText().toString();
+                if(url!=null) {
+
+                if(!url.startsWith("http://") || !url.startsWith("https://")){
+                    url ="http://"+ url;
+                }
+
+                    Uri webpage = Uri.parse(url);
+                    Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+
+                    //the folllwoing thing will ask for browser prefernce
+                    if (intent.resolveActivity(getPackageManager()) != null) {
+                        startActivity(intent);
+                    }
+                }
+            }
+        });
     }
 }
